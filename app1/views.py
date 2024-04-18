0bdf89c62af5ab06203a7131dfa2835d72ccb6b5
IranEme <iranmercado6@gmail.com>
2024-04-17 22:08:25 -0700

    First coomit

---
+++ app1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def HomePage(request):
+    pass
+
+def SignupPage(request):
+    return render (request,'signup.html')
+
+def LoginPage(request):
+    pass
